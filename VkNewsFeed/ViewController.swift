a8fdc593bc6caf0e9c10902313d475d41b072cc6
outsider1703 <simka-simka020697simka-simka1>
2021-01-08 13:58:52 +0200

    Initial Commit

---
+++ VkNewsFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VkNewsFeed
+//
+//  Created by Macbook on 08.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
